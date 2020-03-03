84559bcd43699b9bcb8dedc07de7d6851c2ea52d
Bart Busschots <opensource@bartificer.net>
2020-03-03 00:56:23 +0000

    PBS89 Solution — finished re-factoring Cards

--- instalmentResources/pbs92/pbs89-challengeSolution/index.js
+++ instalmentResources/pbs92/pbs89-challengeSolution/index.js
@@ -65,22 +65,17 @@ async function initCurrencyConverter(){
 	$currencyControls.empty().append(biuldCurrencySelectionFormUI());
 	
 	// load all the currency cards
-	//$cards.empty().append(buildCurrencyCards());
-	
-	// load the placeholders for the currency cards into the page
-	//$cards.empty().append(buildCurrencyCardPlaceholders());
+	$cards.empty().append(buildCurrencyCardCols());
 	
 	// add the New Card Form into the page
-	//$cards.append(buildNewCardFormUI());
+	$cards.append(buildNewCardFormUI());
 	
 	// load the cards for the default currencies
-	//for(const curCode of SORTED_CURRENCY_CODES){
-	//	if(CURRENCIES[curCode].defaultCard){
-	//		showCurrencyCard(curCode, true).catch(function(e){
-	//			console.error(`failed to show '${curCode}'`, e);
-	//		});
-	//	}
-	//}
+	for(const curCode of SORTED_CURRENCY_CODES){
+		if(CURRENCIES[curCode].defaultCard){
+			showCurrencyCard(curCode, true);
+		}
+	}
 }
 
 $(async function(){
@@ -241,10 +236,8 @@ function buildNewCardFormUI(){
 		// get the selected currency
 		const curCode = $('select', $form).val();
 			
-		// show the requested card (avoid unhandled promise rejection)
-		showCurrencyCard(curCode).catch(function(e){
-			console.error(`failed to show '${curCode}'`, e);
-		});		
+		// show the requested card
+		showCurrencyCard(curCode);		
 	});
 	
 	// return the form
@@ -289,42 +282,36 @@ function buildCurrencyCardCol(curCode){
 		}
 	}
 	
-	// LEFT OFF HERE!!!
-	
-	//// add a click handler to the close button
-	//$('button.close', $card).click(function(){
-	//	// hide the card
-	//	$currencyCardCol(curCode).hide();
-	//	
-	//	// update the add card select
-	//	updateAddCardSelectOptions();
-	//});
+	// add a click handler to the close button
+	$('button.close', $card).click(function(){
+		hideCurrencyCard(curCode);
+	});
 
-	//// add an input handler to the base amount text box
-	//$('input.baseAmount', $card).on('input', function(){
-	//	// convert the DOM object for the input to a jQuery object
-	//	const $input = $(this);
-	//	
-	//	// get a reference to the card's form
-	//	const $form = $input.closest('form');
-	//	
-	//	// enable the display of the validation state on the form
-	//	$form.addClass('was-validated');
-	//	
-	//	// read the base amount for the form
-	//	let baseAmount = $input.val();
-	//	
-	//	// default to 1 if invalid
-	//	if($input.is(':invalid')){
-	//		baseAmount = 1;
-	//	}
-	//	
-	//	// remove a trailing dot if present ('4.' to '4')
-	//	baseAmount = String(baseAmount).replace(/[.]$/, '');
-	//	
-	//	// do the conversion
-	//	updateCardConversions(curCode, baseAmount);
-	//});
+	// add an input handler to the base amount text box
+	$('input.baseAmount', $card).on('input', function(){
+		// convert the DOM object for the input to a jQuery object
+		const $input = $(this);
+		
+		// get a reference to the card's form
+		const $form = $input.closest('form');
+		
+		// enable the display of the validation state on the form
+		$form.addClass('was-validated');
+		
+		// read the base amount for the form
+		let baseAmount = $input.val();
+		
+		// default to 1 if invalid
+		if($input.is(':invalid')){
+			baseAmount = 1;
+		}
+		
+		// remove a trailing dot if present ('4.' to '4')
+		baseAmount = String(baseAmount).replace(/[.]$/, '');
+		
+		// do the conversion
+		updateCardConversions(curCode, baseAmount);
+	});
 	
 	// return the card
 	return $card;
@@ -341,7 +328,14 @@ function buildCurrencyCardCols(){
 	
 	// build and store each card
 	for(const baseCurCode of SORTED_CURRENCY_CODES){
-		$cards = $cards.add(buildCurrencyCardCol(baseCurCode));
+		// generate a card for the currency
+		const $card = buildCurrencyCardCol(baseCurCode);
+		
+		// hide the card
+		$card.hide();
+		
+		// append the card into the collection of cards
+		$cards = $cards.add($card);
 	}
 	
 	// return the placeholder cards
@@ -376,32 +370,18 @@ function buildCurrencyCardPlaceholders(){
 }
 
 //
-// Helper functions
+// Card-view functions
 //
 
 /**
- * Get a jQuery object representing the col for a given currency card.
- * 
- * @param {string} curCode
- * @return {jQuery}
- */
-function $currencyCardCol(curCode){
-	return $(`.currencyCol[data-currency=${curCode}]`);
-}
-
-//
-// LEGACY
-//
-
-/**
- * An asynchronous function to show the card for a given currency.
+ * Show the card for a given currency.
  *
  * @param {string} curCode - The three-letter code for the currency to load.
  * @param {boolean} [skipFocus=false] - Pass a truthy value to skip the
  * focusing of the card after loading.
  * @throws {TypeError} A type error is throw if the currency code is not valid.
  */
-async function showCurrencyCard(curCode, skipFocus){
+function showCurrencyCard(curCode, skipFocus){
 	// validate the currency code
 	curCode = String(curCode).toUpperCase();
 	if(!curCode.match(/^[A-Z]{3}$/)){
@@ -409,144 +389,82 @@ async function showCurrencyCard(curCode, skipFocus){
 	}
 	
 	// get the col for the currency
-	const $curCol = $(`.currencyCol[data-currency='${curCode}']`);
+	const $curCol = $currencyCardCol(curCode);
 	
-	// if the card has already been loaded, just show it and exit
-	if($curCol.data('loaded')){
-		console.debug(`card for '${curCode}' already loaded, so just showing it`);
-		
-		// show the col
-		$curCol.show();
-		
-		// focus the card if appropriate
-		if(!skipFocus) $('input.baseAmount', $curCol).focus();
-		
-		// update the select in the add card form
-		updateAddCardSelectOptions();
-		
-		// end the function
-		return;
-	}
+	// show the col
+	$curCol.show();
 	
-	// if the card is in the process of loading, do nothing
-	if($curCol.data('loading')){
-		console.debug(`card for '${curCode}' already loading, will not start another load`);
-		return;
-	}
+	// mark the card as active
+	$curCol.data('active', true);
 	
-	// if we got here, try render the card
-	$curCol.data('loading', true).show();
-	try{
-		// fetch the data for the currency
-		const curData = await $.ajax({ // could throw Error
-			url: CURRENCY_API_URL,
-			method: 'GET',
-			cache: false,
-			data: {
-				base: curCode
-			}
-		});
-		console.debug(`received currency data for '${curCode}': `, curData);
-		
-		// build the view for the card
-		const cardView = {
-			base: {
-				code: curData.base,
-				...CURRENCIES[curData.base]
-			},
-			rates: []
-		};
-		for(const cc of SORTED_CURRENCY_CODES){
-			if(cc === curData.base) continue; // skip self
-			cardView.rates.push({
-				code: cc,
-				rate: numeral(curData.rates[cc]).format('0,0[.]00'),
-				rawRate: curData.rates[cc],
-				...CURRENCIES[cc]
-			});
-		}
-		console.debug(`generated view for '${curData.base}':`, cardView);
-		
-		// generate the HTML
-		const cardHTML = Mustache.render(TEMPLATES.currencies.displayCard, cardView);
-		
-		// convert the HTML to a jQuery object
-		const $card = $(cardHTML);
-		
-		// hide the currencies that should not be showing
-		for(const cc of SORTED_CURRENCY_CODES){
-			if(!DISPLAY_CURRENCIES[cc]){
-				$(`li.currencyRate[data-currency='${cc}']`, $card).hide();
-			}
-		}
-		
-		// add a click handler to the close button
-		$('button.close', $card).click(function(){
-			// hide the card
-			$curCol.hide();
-			
-			// update the add card select
-			updateAddCardSelectOptions();
-		});
-		
-		// add an input handler to the base amount text box
-		$('input.baseAmount', $card).on('input', function(){
-			// convert the DOM object for the input to a jQuery object
-			const $input = $(this);
-			
-			// get a reference to the card's form
-			const $form = $input.closest('form');
-			
-			// enable the display of the validation state on the form
-			$form.addClass('was-validated');
-			
-			// read the base amount for the form
-			let baseAmount = $input.val();
-			
-			// default to 1 if invalid
-			if($input.is(':invalid')){
-				baseAmount = 1;
-			}
-			
-			// remove a trailing dot if present ('4.' to '4')
-			baseAmount = String(baseAmount).replace(/[.]$/, '');
-			
-			// do the conversion
-			updateCardConversions(curCode, baseAmount);
-		});
-		
-		// replace the placeholder with the card
-		$curCol.empty().append($card);
-		
-		// focus the card if appropriate
-		if(!skipFocus) $('input.baseAmount', $curCol).focus();
-	}catch(err){
-		// log the error's details to the console
-		console.log(`failed to render currency '${curCode}'`, err);
-		
-		// generate an error card
-		const errorCardHTML = Mustache.render(
-			TEMPLATES.errorCard, // template string
-			{ // view
-				message: `Failed to load '${curCode}'.`
-			} 
-		);
-		
-		// show the error card
-		$curCol.empty().append(errorCardHTML);
-		
-		// remove the loading flag from the col
-		$curCol.data('loading', false);
-		
-		// end the function
-		return;
-	}
+	// focus the card if appropriate
+	if(!skipFocus) $('input.baseAmount', $curCol).focus();
 	
-	// mark the col as loaded and 
-	$curCol.data('loading', false).data('loaded', true);
-	
-	// update the rendering of the select for adding cards
+	// update the select in the add card form
 	updateAddCardSelectOptions();
+}
+
+/**
+ * Hide the card for a given currency.
+ *
+ * @param {string} curCode - The three-letter code for the currency to load.
+ * @throws {TypeError} A type error is throw if the currency code is not valid.
+ */
+function hideCurrencyCard(curCode){
+	// validate the currency code
+	curCode = String(curCode).toUpperCase();
+	if(!curCode.match(/^[A-Z]{3}$/)){
+		throw new TypeError(`Invalid country code: ${curCode}`);
+	}
+		
+	// get the col for the currency
+	const $curCol = $currencyCardCol(curCode);
+		
+	// hide the col
+	$curCol.hide();
+	
+	// mark the card as inactive
+	$curCol.data('active', false);
+		
+	// update the select in the add card form
+	updateAddCardSelectOptions();
+}
+
+/**
+ * Update the enabled/disabled state of each option in the
+ * select for adding cards.
+ */
+function updateAddCardSelectOptions(){
+	// get a reference to the options in the select as a jQuery object
+	const $opts = $('#add_currency_sel option');
+	
+	// loop over all the options in the select and enable/disable as needed
+	for(const opt of $opts){
+		// convert DOM object to jQuery object
+		const $opt = $(opt);
+		
+		// get the currency code the option represents
+		const curCode = $opt.val();
+		
+		// get a reference to the col for this currency
+		const $curCol = $currencyCardCol(curCode);
+		
+		// enabled/disable this option based on the visibility
+		// of the matching card
+		$opt.prop('disabled', $curCol.data('active') ? true : false );
+	}
+	
+	// select the first enabled option
+	for(const opt of $opts){
+		// convert DOM object to jQuery object
+		const $opt = $(opt);
+		
+		// if the current option is not disabled, select it and exit the loop
+		if(!$opt.prop('disabled')){
+			$opt.prop('selected', true);
+			break; // end the loop
+		}
+	}
 }
 
 /**
@@ -568,11 +486,6 @@ function updateCardConversions(curCode, baseAmount){
 	
 	// get the col for the currency
 	const $curCol = $(`.currencyCol[data-currency='${curCode}']`);
-	
-	// if the card has not been loaded, throw an error
-	if(!$curCol.data('loaded')){
-		throw new Error(`Currency ${curCode} is not loaded`);
-	}
 	
 	// validate the amount
 	if(!String(baseAmount).match(/^\d+(?:[.]\d+)?$/)){
@@ -604,38 +517,11 @@ function updateCardConversions(curCode, baseAmount){
 }
 
 /**
- * Update the enabled/disabled state of each option in the
- * select for adding cards.
+ * Get a jQuery object representing the col for a given currency card.
+ * 
+ * @param {string} curCode
+ * @return {jQuery}
  */
-function updateAddCardSelectOptions(){
-	// get a reference to the options in the select as a jQuery object
-	const $opts = $('#add_currency_sel option');
-	
-	// loop over all the options in the select and enable/disable as needed
-	for(const opt of $opts){
-		// convert DOM object to jQuery object
-		const $opt = $(opt);
-		
-		// get the currency code the option represents
-		const curCode = $opt.val();
-		
-		// get a reference to the col for this currency
-		const $curCol = $(`.currencyCol[data-currency='${curCode}']`);
-		
-		// enabled/disable this option based on the visibility
-		// of the matching card
-		$opt.prop('disabled', $curCol.is(':hidden') ? false : true );
-	}
-	
-	// select the first enabled option
-	for(const opt of $opts){
-		// convert DOM object to jQuery object
-		const $opt = $(opt);
-		
-		// if the current option is not disabled, select it and exit the loop
-		if(!$opt.prop('disabled')){
-			$opt.prop('selected', true);
-			break; // end the loop
-		}
-	}
+function $currencyCardCol(curCode){
+	return $(`.currencyCol[data-currency=${curCode}]`);
 }
