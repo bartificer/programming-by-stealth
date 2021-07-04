dd1f29c841eebc4467edfbd2dabba4641b85d2c2
Bart Busschots <opensource@bartificer.net>
2021-07-04 16:32:13 +0100

    Made a start on PBS120

---
+++ instalmentResources/pbs120/pbs120-init.sh
@@ -0,0 +1,7 @@
+#! /bin/bash -x
+
+repo=pbs120a
+git clone $repo.bundle $repo
+#git clone --bare $repo.bundle $repo-nas.git
+#git clone $repo-nas.git $repo-desktop
+#git clone $repo-nas.git $repo-laptop
