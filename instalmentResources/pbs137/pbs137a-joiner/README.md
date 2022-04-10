19081df1f5c5237c8188f798eca9e7147925440c
Bart Busschots <opensource@bartificer.net>
2022-04-10 12:29:33 +0100

    Coppied Joiner form PBS 136 and updated to use NPM Module
    This will be the worked example for this instalment

---
+++ instalmentResources/pbs137/pbs137a-joiner/README.md
@@ -0,0 +1,5 @@
+# The PBSJoiner Module
+
+A module for joining lists of strings like a human would.
+
+This module serves as an example in the [Programming by Stealth series](https://pbs.bartificer.net).
---
+++ instalmentResources/pbs137/pbs137a-joiner/src/joiner.test.js
@@ -0,0 +1,65 @@
+import joiner from "./joiner.mjs";
+
+//
+// === Module-level tests ===
+//
+test('Module exports expected value', ()=>{
+   expect(joiner).toBeInstanceOf(Function);
+});
+
+//
+// === Test Default Functionality ===
+//
+describe('test default join (no modifiers)', ()=>{
+  describe.each([
+    ['typical case - 3 arguments', ['a', 'b', 'c'], 'a, b & c'],
+    ['edge case - empty array',    [],              ''        ],
+    ['edge case - one string',     ['a'],           'a'       ],
+    ['edge case - two strings',    ['a', 'b'],      'a & b'   ]
+  ])('with valid data', (desc, input, result)=>{
+    test(desc, ()=>{
+      expect(joiner().join(input)).toBe(result);
+    });
+  });
+  describe.each([
+    ['string',         'pancakes'],
+    ['number',         42],
+    ['boolean',        true],
+    ['plain object',   {a: 'b'}],
+    ['class instance', new Date()]
+  ])('with invalid data', (desc, val)=>{
+    test(`with a ${desc}`, ()=>{
+      expect(()=>{joiner().join(val)}).toThrow(TypeError);
+    });
+  });
+});
+
+//
+// === Test the modifiers ===
+//
+
+describe('test modifiers', ()=>{
+  let testArray;
+  beforeAll(()=>{
+    testArray = ['waffles', 'pancakes', 'popcorn'];
+  });
+
+  test('ampersand modifier', ()=>{
+    expect(joiner().ampersand.join(testArray)).toBe('waffles, pancakes & popcorn');
+  });
+  test('and modifier', ()=>{
+    expect(joiner().and.join(testArray)).toBe('waffles, pancakes and popcorn');
+  });
+  test('or modifier', ()=>{
+    expect(joiner().or.join(testArray)).toBe('waffles, pancakes or popcorn');
+  });
+  test('single quote modifier', ()=>{
+    expect(joiner().quote.join(testArray)).toBe("'waffles', 'pancakes' & 'popcorn'");
+  });
+  test('double quote modifier', ()=>{
+    expect(joiner().doubleQuote.join(testArray)).toBe('"waffles", "pancakes" & "popcorn"');
+  });
+  test('sort modifier', ()=>{
+    expect(joiner().sort.join(testArray)).toBe('pancakes, popcorn & waffles');
+  });
+});
