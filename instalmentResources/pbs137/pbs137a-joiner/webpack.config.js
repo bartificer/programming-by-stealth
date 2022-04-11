b0f2af0ef6d705ccd725d532caec4d3316441760
Bart Busschots <opensource@bartificer.net>
2022-04-11 23:50:05 +0100

    Updated WebPack demo to export an ES6 module as well as a UMD

--- instalmentResources/pbs137/pbs137a-joiner/webpack.config.js
+++ instalmentResources/pbs137/pbs137a-joiner/webpack.config.js
@@ -6,15 +6,32 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default {
-  entry: './src/joiner.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'joiner-universal.js',
-    library: {
-      name: 'joiner',
-      type: 'umd',
-      export: 'default',
+export default [
+    // output a old-stype universal module for use in browsers
+    {
+        entry: './src/joiner.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'joiner-universal.js',
+            library: {
+                name: 'joiner',
+                type: 'umd',
+                export: 'default',
+            },
+        },
     },
-  },
-};
+    // output an ES6 module
+    {
+        entry: './src/joiner.js',
+        experiments: {
+            outputModule: true,
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'joiner-es6.js',
+            library: {
+                type: 'module',
+            },
+        },
+    },
+];
