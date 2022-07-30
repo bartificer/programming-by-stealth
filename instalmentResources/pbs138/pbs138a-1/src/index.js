6a552356246dbaba72d9051bb7cbdfb09cf82968
Bart Busschots <opensource@bartificer.net>
2022-07-30 15:43:19 +0100

    Made a good start on worked example

---
+++ instalmentResources/pbs138/pbs138a-1/src/index.js
@@ -0,0 +1,4 @@
+// a document ready handler in plain JavaScript (no JQuery)
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('world-modifier').innerText = 'Dynamic';
+}, false);
