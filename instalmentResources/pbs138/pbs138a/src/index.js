20c33f65bc60b46874783e7d93269445999db97a
Bart Busschots <opensource@bartificer.net>
2022-08-01 19:54:16 +0100

    Re-factored PBS138 not to include Bootstrap JS

--- instalmentResources/pbs138/pbs138a/src/index.js
+++ instalmentResources/pbs138/pbs138a/src/index.js
@@ -10,9 +10,6 @@ import $ from 'jquery';
 // import Luxon
 import { DateTime } from 'luxon'
 
-// import Bootstrap's Alert plugin
-import { Alert } from 'bootstrap'
-
 // a jQuery document ready handler
 $(()=>{
     // update the greeting to show jQuery ran
@@ -21,7 +18,5 @@ $(()=>{
     // build and add an alert with the loaded time
     const $alert = $('<p>').addClass('alert alert-info alert-dismissible');
     $alert.text(`Page loaded at ${DateTime.now().toLocaleString()}`);
-    $alert.append($('<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>'));
-    new Alert($alert[0]);
     $('.container').append($alert);
 });
