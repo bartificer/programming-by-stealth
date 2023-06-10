b1be834750cc204b17942abfc762ec6c551344d0
Bart Busscots <opensource@bartificer.net>
2023-06-10 17:17:39 +0100

    WIP: PBS 151 challenge solution without bonus

--- instalmentResources/pbs152/pbs151-challengeSolution.sh
+++ instalmentResources/pbs152/pbs151-challengeSolution.sh
@@ -1,29 +1,46 @@
 #!/usr/bin/env bash
 
-# process args
-min=1
-max=10
-usage="Usage: $(basename $0) [-m MIN] [-M MAX] N"
-while getopts ':m:M:' opt
+#
+# === set defaults ===
+#
+start=1    # default to starting at 1
+end=10     # default to ending at 10
+doDebug='' # default to not debugging
+
+#
+# === process args ===
+#
+
+# utility variables
+usage="Usage: $(basename $0) [-s START] [-e END] N"
+intRE='^-?[0-9]+$'
+
+# process optional args & flags
+while getopts ':s:e:d' opt
 do
     case $opt in
-        m)  
-            if echo "$OPTARG" | egrep -q '^-?[0-9]+$' <<<
+        s)  
+            if echo "$OPTARG" | egrep -q "$intRE"
             then
-                min=$OPTARG
+                start=$OPTARG
             else
-                echo "invalid minimum value '$OPTARG'" >&2
+                echo "invalid starting value '$OPTARG', must be a whole number" >&2
+                echo "$usage" >&2
                 exit 2
             fi
             ;;
-        M)
-            if echo "$OPTARG" | egrep -q '^-?[0-9]+$'
+        e)
+            if echo "$OPTARG" | egrep -q "$intRE"
             then
-                max=$OPTARG
+                end=$OPTARG
             else
-                echo "invalid maximum value '$OPTARG'" >&2
+                echo "invalid ending value '$OPTARG', must be a whole number" >&2
+                echo "$usage" >&2
                 exit 2
             fi
+            ;;
+        d)
+            doDebug=1
             ;;
         ?)
             echo "$usage" >&2
@@ -32,16 +49,83 @@ do
     esac
 done
 shift $(echo "$OPTIND-1" | bc)
+
+# process positional arg
 n=$1
-if [[ -z $n ]]
+if [[ -z $n ]] # no first real arg
 then
     echo "$usage" >&2
     exit 1
 fi
-if ! echo "$n" | egrep -q '^-?[0-9]+$'
+if ! echo "$n" | egrep -q "$intRE" # invalid first real arg
 then
     echo "invalid number '$n' first argument must be a whole number" >&2
     exit 2
 fi
 
-echo "$n times tables from $min to $max"
+# if debugging, print values
+if [[ -n $doDebug ]]
+then
+    echo "DEBUG: n       = $n" >&2
+    echo "DEBUG: start   = $start" >&2
+    echo "DEBUG: end     = $end" >&2
+fi
+
+#
+# === Build row format string ===
+#
+
+# calculate maximum lengths for each column when nicely fomatted
+nLen=$(printf "%'d" $n | wc -c | xargs) # character length of the number when formatted
+maxMLen=1 # maximum character length of any formatted multiplier
+maxPLen=1 # maximum character length of any formatted product
+for m in $(seq $start $end)
+do
+    # Multiplier length when nicely formatted
+    mLen=$(printf "%'d" $m | wc -c | xargs)
+    [[ $mLen -gt $maxMLen ]] && maxMLen=$mLen
+
+    # Product length when nicely formatted
+    pLen=$(echo "$n*$m" | bc | xargs printf "%'d" | wc -c | xargs)
+    [[ $pLen -gt $maxPLen ]] && maxPLen=$pLen
+done
+
+# calculate the lenght of the middle piece of the table caps
+capLen=$(echo "8+$nLen+$maxMLen+$maxPLen" | bc)
+
+# if debugging, print the calculated numbers
+if [[ -n $doDebug ]]
+then
+    echo "DEBUG: nLen    = $nLen" >&2
+    echo "DEBUG: maxMLen = $maxMLen" >&2
+    echo "DEBUG: maxPLen = $maxPLen" >&2
+    echo "DEBUG: capLen  = $capLen" >&2
+fi
+
+# build the row format string, and print if debugging
+fString="┃ %'"$nLen"d x %'"$maxMLen"d = %'"$maxPLen"d ┃\n"
+[[ -n $doDebug ]] && echo "DEBUG: fString = $fString" >&2
+
+# build the cap insert, and print if debugging
+capMid=''; for c in $(seq 1 $capLen); do capMid+='━'; done
+[[ -n $doDebug ]] && echo "DEBUG: capMid  = $capMid" >&2
+
+#
+# === print the table ===
+#
+
+# print the top cap row
+printf '┏%s┓\n' $capMid
+
+# print the table body
+for m in $(seq $start $end)
+do
+    # calcuate the product
+    p=$(echo "$n*$m" | bc)
+
+    # print the table row
+    printf "$fString" $n $m $p
+done
+
+# print the bottom cap row
+printf '┗%s┛\n' $capMid 
