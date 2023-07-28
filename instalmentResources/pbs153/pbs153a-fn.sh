99b6e04c1b4af7c6e647e505e8165ee86538b599
Bart Busscots <opensource@bartificer.net>
2023-07-28 17:30:30 +0100

    WIP: start PBS 153

---
+++ instalmentResources/pbs153/pbs153a-fn.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# store the number of times to call the function
+fnCounter=1
+echo "$1" | egrep '^\d+$' -q && fnCounter=$1
+
+# define a POSIX Hello World fuction
+# Arguments: NONE
+hellow () {
+    echo 'Hello World!'
+    echo '(from inside a POSIX function)'
+}
+
+# call simple greeting function the appropriate number of times
+while (( fnCounter > 0 )) # NOTE the arithmetic expression
+do
+    # call the function
+    hellow
+
+    # decrement the counter with an arithmetic expression
+    (( fnCounter-- ))
+
+    # print an empty line unless done
+    (( fnCounter > 0 )) && echo ''
+done
