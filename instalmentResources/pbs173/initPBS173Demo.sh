a86d6f5a2fa1249c806c2ad569ba6219fd69bc3a
Bart Busschots <opensource@bartificer.net>
2024-10-27 17:21:21 +0000

    WIP: made a start on PBS 173 demos

---
+++ instalmentResources/pbs173/initPBS173Demo.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# conditionally create the repos folder
+[[ -e repos ]] || mkdir repos
+
+# remove any old coppies of the repos
+rm -rf ./repos/pbscorp-brand
+rm -rf ./repos/pbscorp-app1
+
+# clone the bundles into the repos folder
+git clone ./pbscorp-brand.bundle ./repos/pbscorp-brand
+git clone ./pbscorp-app1.bundle ./repos/pbscorp-app1
+
+# update the submodules
+(cd ./repos/pbscorp-app1 && git -c protocol.file.allow=always submodule update)
