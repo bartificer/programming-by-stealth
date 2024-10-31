95895f96909cd8ebf8e8cf86e9ffb1032db3f932
Bart Busschots <opensource@bartificer.net>
2024-10-31 01:23:10 +0000

    Chore: refactor PBS 173 example folder structure

--- instalmentResources/pbs173/initPBS173Demo.sh
+++ instalmentResources/pbs173/initPBS173Demo.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 set -x
 
-# conditionally create the repos folder
-[[ -e repos ]] || mkdir repos
+# conditionally create the 'remote' repos folder
+[[ -e remote-repos ]] || mkdir remote-repos
 
-# remove any old coppies of the repos
-rm -rf ./repos/pbscorp-brand
-rm -rf ./repos/pbscorp-app1
+# remove any old copies of the 'remote' repos
+rm -rf ./remote-repos/pbscorp-brand
+rm -rf ./remote-repos/pbscorp-app1
 
-# clone the bundles into the repos folder
-git clone ./pbscorp-brand.bundle ./repos/pbscorp-brand
-git clone ./pbscorp-app1.bundle ./repos/pbscorp-app1
+# clone the bundles into the 'remote' repos folder
+git clone ./pbscorp-brand.bundle ./remote-repos/pbscorp-brand
+git clone ./pbscorp-app1.bundle ./remote-repos/pbscorp-app1
 
 # update the submodules
-(cd ./repos/pbscorp-app1 && git -c protocol.file.allow=always submodule update)
+(cd ./remote-repos/pbscorp-app1 && git -c protocol.file.allow=always submodule update)
+
+# create the working folders used in examples
+[[ -e pc-app1Dev ]] || mkdir pc-app1Dev
+[[ -e pc-app2Dev ]] || mkdir pc-app2Dev
