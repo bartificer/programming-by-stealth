165b484f0465a67e7b38c0a3eaa6877bace317e2
Bart Busschots <opensource@bartificer.net>
2024-11-01 01:11:37 +0000

    WIP: continue work setting up the scenarios

--- instalmentResources/pbs173/initPBS173Demo.sh
+++ instalmentResources/pbs173/initPBS173Demo.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 set -x
 
-# conditionally create the 'remote' repos folder
-[[ -e remote-repos ]] || mkdir remote-repos
+# conditionally create the folders representing the pretend computers, and empty them
+for folder in remote-repos pc-app1Dev pc-app2Dev pc-brandDesigner
+do
+    [[ -e $folder ]] || mkdir $folder
+    rm -rf $folder/*
+done
 
 # remove any old copies of the 'remote' repos
 rm -rf ./remote-repos/pbscorp-brand
@@ -12,9 +16,5 @@ rm -rf ./remote-repos/pbscorp-app1
 git clone ./pbscorp-brand.bundle ./remote-repos/pbscorp-brand
 git clone ./pbscorp-app1.bundle ./remote-repos/pbscorp-app1
 
-# update the submodules
-(cd ./remote-repos/pbscorp-app1 && git -c protocol.file.allow=always submodule update)
-
-# create the working folders used in examples
-[[ -e pc-app1Dev ]] || mkdir pc-app1Dev
-[[ -e pc-app2Dev ]] || mkdir pc-app2Dev
+# update the submodule in app 1
+(cd ./remote-repos/pbscorp-app1 && git submodule init && git -c protocol.file.allow=always submodule update)
