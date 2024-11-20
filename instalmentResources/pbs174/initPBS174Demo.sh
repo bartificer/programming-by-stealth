0d89e5d856fe79632881df777aef05332458c01e
Bart Busschots <opensource@bartificer.net>
2024-11-20 23:57:52 +0000

    WIP: good progress on PBS 174
    Summarised the scenario reminder and added a first draft of the init script.

---
+++ instalmentResources/pbs174/initPBS174Demo.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -x
+
+# conditionally create the folders representing the pretend computers, and empty them
+for folder in remote-repos pc-app1Dev pc-app2Dev pc-brandDesigner
+do
+    [[ -e $folder ]] || mkdir $folder
+    rm -rf $folder/*
+done
+
+# remove any old copies of the 'remote' repos
+for repo in pbscorp-brand pbscorp-app1 pbscorp-app2
+do
+    rm -rf "./remote-repos/$repo"
+done
+
+# clone the bundles into the 'remote' repos folder
+for repo in pbscorp-brand pbscorp-app1 pbscorp-app2
+do
+    git clone --bare "./$repo.bundle" "./remote-repos/$repo.git"
+done
+
+# clone the repos into each work's 'PC'
+(cd ./pc-brandDesigner && git clone ../remote-repos/pbscorp-brand.git)
+for app in app1 app2
+do
+    ( \
+        cd "./pc-${app}Dev" && \
+        git "clone ../remote-repos/pbscorp-$app.git" && \
+        git submodule init \
+        git git -c protocol.file.allow=always submodule update
+    )
+done
